db73d50ca606a7152183c770808b78c52c6990e8
Tanya Shemet <tanya_shemet@Tanyas-MacBook-Pro-2.local>
2024-07-12 06:46:17 -0700

    set service account file extension

--- board/pages.py
+++ board/pages.py
@@ -14,7 +14,7 @@ app.config['SESSION_TYPE'] = SESSION_TYPE
 app.secret_key = os.getenv('SECRET_KEY')
 
 def allowed_file(filename):
-    allowed_extensions = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+    allowed_extensions = {'json'}
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in allowed_extensions
 
